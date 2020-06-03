9667c12fff4d7cf4a0b85cf7c133d4f51f6a1aa5
JeanLinux <jeanc.aldea1@gmail.com>
2020-06-03 16:36:47 -0400

    WORKS

--- models/contact.go
+++ models/contact.go
@@ -1,6 +1,8 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 //Contact modelo para contactos
 type Contact struct {
@@ -10,4 +12,6 @@ type Contact struct {
 	Telefono    string `json:"telefono" gorm:"size:20"`
 	Direccion   string `json:"email"`
 	Descripcion string `json:"descripcion" gorm:"type:TEXT"`
+	// CreatedAt   time.Time `db:"created_at"`
+	// UpdatedAt   time.Time `db:"updated_at"`
 }
